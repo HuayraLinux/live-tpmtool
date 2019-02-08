9f9ececc42b786dcb71f86fcd958fa224c77ef77
Fernando Toledo <ragnarok@docksud.com.ar>
2019-02-08 11:13:19 -0300

    Agrega timestap al nombre la iso. Elimina el inotify monitor que no anduvo. Corecciones varias al tpm monitor..

--- tpm4/config/includes.chroot/home/usuario/inotify-monitor.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-inoticoming --foreground /opt/TheftDeterrentclient/client/Download echo {} \;
--- tpm4/config/includes.chroot/home/usuario/tpm-monitor.js
+++ tpm4/config/includes.chroot/home/usuario/tpm-monitor.js
@@ -1,15 +1,18 @@
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
+const Gtk = imports.gi.Gtk;
 
-const ICO_UNKNOWN = '0';
-const ICO_CONNECTED_INACTIVE = '1';
-const ICO_DISCONNECTED_INACTIVE_URI = '2';
-const ICO_ABOUT_EXPIRE = '3';
-const ICO_DISCONNECTACTIVE = '4';
-const ICO_DOWNLOADING = '5';
-const ICO_PERMANENT = '6';
-const ICO_PROTECTED = '7';
-const ICO_UPGRADING = '8';
+const EVT_UNKNOWN = '0';
+const EVT_CONNECTED_INACTIVE = '1';
+const EVT_DISCONNECTED_INACTIVE_URI = '2';
+const EVT_ABOUT_EXPIRE = '3';
+const EVT_DISCONNECTACTIVE = '4';
+const EVT_DOWNLOADING = '5';
+const EVT_PERMANENT = '6';
+const EVT_PROTECTED = '7';
+const EVT_UPGRADING = '8';
+
+let count = 0;
 
 const TDIface = '<node> \
 <interface name="com.intel.cmpc.td.client"> \
@@ -30,6 +33,8 @@ const TDIface = '<node> \
 </interface> \
 </node>';
 
+Gtk.init(null);
+
 //Create the remote object, based on the correct parh and bus name
 const TDProxy = Gio.DBusProxy.makeProxyWrapper(TDIface);
 
@@ -40,17 +45,58 @@ TDclient.connectSignal("TDInfoChanged", TDInfoChanged);
 
 print ("Iniciando TPM Monitor...");
 
-function TDInfoChanged(proxy,sender,msg) {
-    if(msg != null) {
-	print ("Estado: " + msg[0].id);
-	print ("MSG: " + msg[0].message);
-	print ("HDR: " + msg[0].header);
+function TDInfoChanged(proxy, sender, msg)
+{
+    if (msg == null)
+        return;
 
-	if (msg[0].message.indexOf("2020") !== -1 ) {
-	    print ("Certificado aplicado. Apagar el equipo");
-	}
+    print ("Estado: " + msg[0].id);
+    print ("MSG: " + msg[0].message);
+    print ("HDR: " + msg[0].header);
+    print ("Evt count:" + count);
+
+    if (msg[0].id == EVT_UNKNOWN ||
+        msg[0].id == EVT_CONNECTED_INACTIVE ||
+        msg[0].id == EVT_DISCONNECTED_INACTIVE_URI ||
+        msg[0].id == EVT_DISCONNECTACTIVE) {
+        print ("Problemas de conexion. Ignorando evento");
+        return;
+    }
+
+    count = count + 1;
+
+    if (msg[0].message.indexOf("2020") !== -1) {
+        //aviso.show();
+        print ("Certificado aplicado. Apagando el equipo");
+        GLib.spawn_command_line_sync('sudo poweroff');
+    }
+    else if (count >= 4) {
+        print ("Reiniciando para aplicar certificado.");
+        GLib.spawn_command_line_sync('sudo reboot');
+    }
+
+}
+
+let aviso = new Gtk.MessageDialog ({
+    modal: true,
+    buttons: Gtk.ButtonsType.OK,
+    message_type: Gtk.MessageType.WARNING,
+    text: "Certificado aplicado, el equipo se apagará." });
+
+aviso.connect('response', (this, _response_cb.bind(this)));
+
+function _response_cb (dialog, response_id)
+{
+    switch (response_id) {
+        case Gtk.ResponseType.OK:
+            print ("Certificado aplicado. Apagando el equipo.");
+            GLib.spawn_command_line_sync('sudo poweroff');
+            break;
+        case Gtk.ResponseType.DELETE_EVENT:
+            print ("Dialog closed or cancelled.\n");
+            aviso.hide();
+            break;
     }
 }
 
-let loop = new GLib.MainLoop(null, false);
-loop.run();
+Gtk.main();
